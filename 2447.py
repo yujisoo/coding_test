935a7ddafa1cf70db927aa9efeed11aee8f2f5d2
yujisoo <sdbwltns@gmail.com>
2022-12-25 23:33:14 +0900

    별 찍기 - 10

--- 2447.py
+++ 2447.py
@@ -3,6 +3,18 @@ input = sys.stdin.readline
 
 n = int(input())
 
-for i in range(n):
-    for j in range(n):
-        if 
+def star(n) :
+    if n==1:
+        return ["*"]
+    a = star(n//3)
+    l = []
+    
+    for i in a:
+        l.append(i*3)
+    for i in a:
+        l.append(i+" "*(n//3)+i)
+    for i in a:
+        l.append(i*3)
+    return l
+    
+print("\n".join(star(n)))
