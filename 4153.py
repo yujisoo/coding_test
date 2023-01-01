b306b381543bb2b8c5d7ff385b00279fa6950d5c
yujisoo <sdbwltns@gmail.com>
2023-01-01 19:27:26 +0900

    직각삼각형

---
+++ 4153.py
@@ -0,0 +1,11 @@
+while True:
+    a = list(map(int,input().split()))
+    
+    if a[0]==0 and a[1]==0 and a[2]==0:
+        break
+
+    a.sort()
+    if a[2]**2 == a[0]**2+a[1]**2:
+        print("right")
+    else:
+        print("wrong")
