e94ebe149a3f805fc973fc134a34bf7f5bb36aeb
yujisoo <sdbwltns@gmail.com>
2023-01-10 23:32:07 +0900

    알고리즘 수업 - 피보나치 수 1

---
+++ 24416.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cnt1 = 0
+cnt2 = 0
+
+def fib(n):
+    global cnt1
+    if n==1 or n==2:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+dp = [0]*(n+1)
+dp[1]=1
+dp[2]=1
+def fibonacci(n):
+    cnt = 0
+    for i in range(3,n+1):
+        cnt+=1
+        dp[i]=dp[i-1]+dp[i-2]
+    return cnt
+
+
+print(fib(n),fibonacci(n))
