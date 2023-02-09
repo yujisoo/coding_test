9c607ca9abaa28e205443d121b28d0487fc29733
yujisoo <sdbwltns@gmail.com>
2023-02-09 23:06:24 +0900

    퇴사

---
+++ 14501.py
@@ -0,0 +1,14 @@
+n = int(input())
+data = [[0,0] for _ in range(n)]
+dp = [0]*(n+1)
+
+for i in range(n):
+    a,b = map(int,input().split())
+    data[i]=[a,b]
+
+for i in range(n):
+    for j in range(i+data[i][0],n+1):
+        if dp[j]<dp[i]+data[i][1]:
+            dp[j] = dp[i]+data[i][1]
+            
+print(max(dp))
